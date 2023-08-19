df4ce2f0226c51a1c74d4f7237f8a4876c2e81f5
Erin Stephens <erebstephens@gmail.com>
2023-08-19 11:25:42 -0500

    park views complete for CRUD

--- parktrackerapi/views/park.py
+++ parktrackerapi/views/park.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from parktrackerapi.models import Park
+from parktrackerapi.models import Park, User
 
 
 class ParkView(ViewSet):
@@ -29,9 +29,40 @@ class ParkView(ViewSet):
         parks = Park.objects.all()
         serializer = ParkSerializer(parks, many=True)
         return Response(serializer.data)
+      
+    def create(self, request):
+        """ POST"""
+        user = User.objects.get(uid=request.data["userId"])
+        park = Park.objects.create(
+          user=user,
+          park_name=request.data["parkName"],
+          image_url=request.data["imageUrl"],
+          location=request.data["location"],
+          park_type=request.data["parkType"],
+        )
+        serializer = ParkSerializer(park)
+        return Response(serializer.data)
+      
+    def update(self, request, pk):
+        """PUT request"""
+        park = Park.objects.get(pk=pk)
+        park.park_name = request.data["parkName"]
+        park.image_url = request.data["imageUrl"]
+        park.location = request.data["location"]
+        park.park_type = request.data["parkType"]
+        
+        park.save()
+        
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        park = Park.objects.get(pk=pk)
+        park.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class ParkSerializer(serializers.ModelSerializer):
     """JSON serializer for parks"""
     class Meta: 
         model = Park
         fields = ('id', 'user', 'park_name', 'image_url', 'location', 'park_type')
+        depth = 1
